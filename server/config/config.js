c369b608d39f73e03f28a52dc5f32d91cb36a235
Roger Arcarons Llonc <roger@arcarons.cat>
2018-06-08 17:22:13 +0200

    Variable d'entorn MONGO a Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-user:1a2b3c@ds247270.mlab.com:47270/cafe-curs_node'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
