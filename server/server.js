cd3f38e588627a1f9ad53b3b8c12d630c550606c
Roger Arcarons Llonc <roger@arcarons.cat>
2018-06-25 17:13:55 +0200

    Subida de archivos

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const path = require('path')
 const app = express();
 
 const bodyParser = require('body-parser');
-
+// parse application/json
+app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
+
 
 // Habilitar la carpeta public
 app.use(express.static(path.resolve(__dirname, '../public')));
